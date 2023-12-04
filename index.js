7495c0b36e6a528a0dc72f72a03e2da4b8ee59dd
Zeenat Abrar <zeenatabrar50@gmail.com>
2023-12-04 20:02:04 +0530

    done

--- index.js
+++ index.js
@@ -1,105 +1,75 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// app.use(bodyParser.json());
-
-// mongoose.connect('mongodb+srv://zeenat:abrar@cluster0.rqsbzxg.mongodb.net/?retryWrites=true&w=majority', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// const userSchema = new mongoose.Schema({
-//   name: String,
-//   email: String,
-//   phone: String,
-//   label: String,
-//   booked_slots: [{ type: String }],
-// });
-
-// const User = mongoose.model('User', userSchema);
-
-// app.get('/users', async (req, res) => {
-//   try {
-//     const users = await User.find();
-//     res.json(users);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
-
-// app.post('/users', async (req, res) => {
-//   try {
-//     const newUser = new User(req.body);
-//     await newUser.save();
-//     res.json(newUser);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
-
-// app.put('/users/:id', async (req, res) => {
-//   try {
-//     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-//     res.json(updatedUser);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
-
-// app.delete('/users/:id', async (req, res) => {
-//   try {
-//     await User.findByIdAndDelete(req.params.id);
-//     res.json({ message: 'User deleted successfully' });
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
-
-// app.get('/users/search', async (req, res) => {
-//   const { term } = req.query;
-
-//   try {
-//     const users = await User.find({ name: { $regex: term, $options: 'i' } });
-//     res.json(users);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
-
-
-const express = require("express");
-const { connection } = require("./db");
-
-const { postRouter } = require("./Routes/Posts.route");
-const cors=require("cors")
-
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(cors())
+app.use(bodyParser.json());
 
+mongoose.connect('mongodb+srv://zeenat:abrar@cluster0.rqsbzxg.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use("/Appointments", postRouter);
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  label: String,
+  booked_slots: [{ type: String }],
+});
 
+const User = mongoose.model('User', userSchema);
 
+app.get('/contacts', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-app.listen(8081, async() => {
-    try{
-      await connection;
-      console.log("Connected to the DB");
-      console.log(`Running at 8080 port`);
-    }catch(err){
-        console.log(err);
-    }
-})
+app.post('/contacts', async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.put('/contacts/:id', async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.delete('/contacts/:id', async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Contact deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/contacts/search', async (req, res) => {
+  const { term } = req.query;
+
+  try {
+    const users = await User.find({ name: { $regex: term, $options: 'i' } });
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
